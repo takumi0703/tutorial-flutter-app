ca3bfce8694d4c9d9cd18a0169097f19d712b5d1
takumi otani <takumiotani@takuminonotobukkukonpyuta.local>
2024-09-30 08:34:29 +0900

    modelクラス追加

---
+++ lib/models/user.dart
@@ -0,0 +1,17 @@
+class User {
+  // コンストラクタ
+  User({
+    required this.id,
+    required this.profileImageUrl,
+  });
+  // プロパティ
+  final String id;
+  final String profileImageUrl;
+
+  factory User.fromJson(dynamic json) {
+    return User(
+      id: json['id'] as String,
+      profileImageUrl: json['profileImageUrl'] as String,
+    );
+  }
+}
