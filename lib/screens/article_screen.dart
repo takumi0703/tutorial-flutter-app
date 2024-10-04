02dfbf09c87f628e08ed38c6857c22c175eacb5c
takumi otani <takumiotani@takuminonotobukkukonpyuta.local>
2024-10-04 09:22:31 +0900

    詳細画面完了

--- lib/screens/article_screen.dart
+++ lib/screens/article_screen.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:qiita_search_app/models/article.dart';
 
 class ArticleScreen extends StatefulWidget {
-  const ArticleScreen({super.key});
+  const ArticleScreen({
+    super.key,
+    required this.article,
+  });
+
+  final Article article;
 
   @override
   State<ArticleScreen> createState() => _ArticleScreenState();
 }
 
 class _ArticleScreenState extends State<ArticleScreen> {
+  late WebViewController controller = WebViewController()
+    // .. は、連鎖的に実行するためのもの
+    ..loadRequest(
+      Uri.parse(widget.article.url),
+    );
+
   @override
 
   Widget build(BuildContext context) {
@@ -15,7 +28,7 @@ class _ArticleScreenState extends State<ArticleScreen> {
       appBar: AppBar(
         title: const Text('Article Page'),
       ),
-      body: null,
+      body: WebViewWidget(controller: controller),
     );
   }
 }
