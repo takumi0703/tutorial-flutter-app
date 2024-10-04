c21130e704be7d85057aba2584e06d023c3ce924
takumi otani <takumiotani@takuminonotobukkukonpyuta.local>
2024-10-04 09:14:13 +0900

    articleページ作成

---
+++ lib/screens/article_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ArticleScreen extends StatefulWidget {
+  const ArticleScreen({super.key});
+
+  @override
+  State<ArticleScreen> createState() => _ArticleScreenState();
+}
+
+class _ArticleScreenState extends State<ArticleScreen> {
+  @override
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Article Page'),
+      ),
+      body: null,
+    );
+  }
+}
